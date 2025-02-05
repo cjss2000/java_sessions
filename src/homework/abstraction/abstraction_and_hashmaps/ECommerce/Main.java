a69dfc7b7938bf0ce66e6d330bded729482753cc
cjss2000 <cjss2000@gmail.com>
2025-02-05 09:08:53 +0100

    Updated code, testing now

--- src/homework/abstraction/abstraction_and_hashmaps/ECommerce/Main.java
+++ src/homework/abstraction/abstraction_and_hashmaps/ECommerce/Main.java
@@ -2,6 +2,8 @@ package homework.abstraction.abstraction_and_hashmaps.ECommerce;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hey");
+        ExpressOrder book = new ExpressOrder("E045", "Mike Stevens", 120.00, 5.00);
+        book.applyDiscount();
+        book.getFinalTotal();
     }
 }
